10a50ecc93fd4efdbe670503029dea771b22adf8
TrevorSuper <129561006+TrevorSuper@users.noreply.github.com>
2024-06-04 23:35:19 -0400

    Clicking create account now generates a sha256sum from the user input into the password field

--- js/login.js
+++ js/login.js
@@ -41,6 +41,7 @@ document.getElementById('codeForm').addEventListener('submit', function(event) {
 
     // Placeholder until database
     if (code === '123456') {
+        //setCookie("email", "test@example.com", "password", "008c70392e3abfbd0fa47bbc2ed96aa99bd49e159727fcba0f2e6abeb3a9d601", 0.25);
         window.location.href = 'welcome.html';
         // Redirect to another page or perform other actions
     } else {
@@ -51,3 +52,13 @@ document.getElementById('codeForm').addEventListener('submit', function(event) {
 document.getElementById('createAccountButton').addEventListener('click', function() {
     window.location.href = 'create_account.html';
 });
+
+/*
+function setCookie(email_literal, email_actual, password_literal, password_actual, daysToLive){
+    const date = new Date();
+    date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${email_literal}=${email_actual};`;
+    document.cookie = `${password_literal}=${password_actual};`;
+    //console.log(document.cookie); // debug log to read the cookies
+}*/
