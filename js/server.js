4f271d6f94746d4f4f40324b473aa0d13dcdfb5b
LoganTemp <logantemplin@oakland.edu>
2024-06-11 17:08:15 -0400

    Added email for verification code
    The email works for sending verification code, next step is to make the 6 digit code random

--- js/server.js
+++ js/server.js
@@ -16,6 +16,14 @@ let transporter = nodemailer.createTransport({
   }
 });
 
+transporter.verify(function(error, success) {
+  if (error) {
+    console.error('Error with transporter verification:', error);
+  } else {
+    console.log('Server is ready to take our messages');
+  }
+});
+
 app.post('/send-verification-email', (req, res) => {
   const { email } = req.body;
   console.log(`Received request to send verification email to: ${email}`);
@@ -35,6 +43,28 @@ app.post('/send-verification-email', (req, res) => {
     }
     console.log('Email sent: ', info.response);
     res.status(200).send('Verification email sent');
+  });
+});
+
+app.post('/send-login-verification-email', (req, res) => {
+  const { email } = req.body;
+  console.log(`Received request to send login verification email to: ${email}`);
+
+  let mailOptions = {
+    from: '"Neutron Pass" <neutron.pass.master@gmail.com>',
+    to: email,
+    subject: 'New Login Detected',
+    text: 'A new login has been detected. Verification code: 123456',
+    html: '<b>A new login has been detected. Verification code: 123456</b>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error occurred: ', error.message);
+      return res.status(500).send({ error: error.message });
+    }
+    console.log('Email sent: ', info.response);
+    res.status(200).send('Login verification email sent');
   });
 });
 
